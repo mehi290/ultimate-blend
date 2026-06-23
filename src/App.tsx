9811f020575f30fcfe14a8fcc060652dd459cff7
mehi290 <luxeutopia6@gmail.com>
2026-06-23 11:53:32 +0400

    chore: integrate vercel speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // Pages
 import Index from "./pages/Index.tsx";
@@ -42,6 +43,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <SpeedInsights />
       <BrowserRouter>
         <AnalyticsTracker />
         <Routes>
